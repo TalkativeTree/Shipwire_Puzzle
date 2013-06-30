010742e82b139f868795cfc3d0a9ed3f7e7c0c8f
Ben Angel <benjamin.b2@gmail.com>
2013-06-29 23:27:55 -0700

    reorganized methods and separated responsibilities for schedule into schedule organizer
    Realized I should separate out the left and the width into an order view class referencing the Order. Will rewrite

--- application.js
+++ application.js
@@ -12,82 +12,115 @@ function Order(orderId,packingStart,duration){
   this.duration     = duration;
   this.left         = 0;
   this.width        = 800;
+  this.parentOrders = [];
 }
 
-
-Order.prototype.shrinkToFit = function(){
-  factor = 800 / this.width
-}
+Order.prototype = {
+     convert: function(object){
+                this.orderId       = object.orderId;
+                this.packingStart  = object.packingStart;
+                this.duration      = object.duration;
+                this.template = '<div class="order'+ this.orderId +' style="height:' + this.duration +
+                                'px; position:absolute; top:' + this.packingStart + 'px; left:'+ this.left +
+                                '; width:'+ this.width +'px"> #' + this.orderId + '<div>';
+              },
+  addParents: function(orders){
+                this.parentOrders.push(orders);
+                console.log(orders);
+                console.log(this.parentOrders);
+              }
+};
 
 function Schedule(orders){
   this.orders = orders || [];
+  this.node   = $('.schedule');
 }
 
 Schedule.prototype = {
-               add: function(order){
-                      this.orders.push(order);
-                    },
-      shrinkOrders: function(exisitingOrder, newOrder){
-                      var factor   = 800 / exisitingOrder.width;
-                      var ratio    = (factor / factor + 1);
-                      var newWidth = exisitingOrder.width * ratio;
+            add: function(order){
+                   this.orders.push(order);
+                 },
+  updateParents: function(order){
+                   for(var p = 0; p < order.parentOrders.length; p++){
+                     var parentOrder = order.parentOrders[p];
+                   };
+                 // for(var i =0; i < todaysSchedule.orders.length; i++){
+                 //   newOrder = todaysSchedule.orders[i];
+                 //   if(newOrder.orderId===5){
+                 //     todaysSchedule.checkTimeOverlap(newOrder);
+                 //   }
+                 }
+};
 
-                      exisitingOrder.width = newWidth;
-                      newOrder.width       = newWidth;
-                      newOrder.left        = newWidth;
+
+var ScheduleOrganizer = {
+              init: function(orders){
+                      console.log(this);
+                      this.convertAll(orders);
+                      for(var i = 0; i < this.schedule.orders.length; i++){
+                        var queuedOrder = this.schedule.orders[i];
+                        this.checkTimeOverlap(queuedOrder);
+                        this.schedule.add(queuedOrder);
+                        }
                     },
-        checkRange: function(exisitingOrder, newOrder){
-                      var oldStart = exisitingOrder.packingStart;
-                      var oldEnd   = exisitingOrder.packingStart + exisitingOrder.duration;
-                      var newStart = newOrder.packingStart;
-                      var newEnd = newOrder.packingStart + newOrder.duration;
+    convertToOrder: function(order){
+                      var newOrder = new Order;
+                      newOrder.convert(order);
+                      return newOrder;
+                    },
+        convertAll: function(myJSONOrders){
+                      myParsedOrders = JSON.parse(myJSONOrders);
+                      var newOrders =  myParsedOrders.map(this.convertToOrder);
+                      this.schedule = new Schedule(newOrders);
+                    },
+  checkTimeOverlap: function(order){
+                      for(var currentOrderIndex = 0; currentOrderIndex < (this.schedule.orders.length); currentOrderIndex++){
+                        allOrders = this.orders;
+                        var existingOrder = this.schedule.orders[currentOrderIndex] || new Order;
+                        this.checkRange(existingOrder, order);
+                      }
+                    },
+        checkRange: function(existingOrder, newOrder){
+                       // console.log(existingOrder);
+                       // console.log(newOrder);
+                       // debugger;
+                       var oldStart = existingOrder.packingStart;
+                       var oldEnd   = existingOrder.packingStart + existingOrder.duration;
+                       var newStart = newOrder.packingStart;
+                       var newEnd = newOrder.packingStart + newOrder.duration;
 
                       if(oldStart <= newStart && newStart <= oldEnd ||
                          oldStart <= newEnd && newEnd <= oldEnd)
                         {
-                          shrinkOrders(exisitingOrder, newOrder)
-                        };
+                          this.resizeParents(existingOrder, newOrder);
+                          // this.checkTimeOverlap(existingOrder);
+                        }
                     },
-  checkTimeOverlap: function(order){
-                      this.orders.each();
-                    },
-}
+       resizeOrder: function(existingOrder, newOrder){
+                      var baseWidth = newOrder.parentOrders[0] || this.width;
+                      var factor   = 800 / baseWidth;
+                      var widthRatio    = factor / (factor + 1);
+                      console.log(ratio);
+                      var newWidth = existingOrder.width * ratio;
+                      console.log(newWidth);
 
-function renderOrder(element, index, array){
-    console.log("this worked");
-    var orderTemplate = '<div class="order" style="height =' + element.duration +'px;top=' +
-                        element.packingStart + 'px;float:left;">' + element.orderId + '<div>';
-    $('.schedule').append(orderTemplate);
-}
-var Renderer = {
-           // init: function(orders){
-           //         myOrders.each(convertOrder);
-           //         this.mySchedule = new Schedule(orders);
-           //       },
-    renderOrder: function(element, index, array){
-                   console.log("this worked");
-                   var orderTemplate = '<div class="order" style="height =' + element.duration +'px;top=' +
-                       element.packingStart + 'px;float:left;">' + element.orderId + '<div>';
-                   $('.schedule').append(orderTemplate);
-                 },
-  displayOrders: function(){
-                   console.log('displayOrders');
-                   this.mySchedule.orders.forEach(renderOrder)
-                 }
+                      existingOrder.width = newWidth;
+                      newOrder.width      = newWidth;
+                      newOrder.left       = (newWidth + existingOrder.left);
+                    },
+     resizeParents: function(existingOrder, newOrder){
+                      newOrder.addParents(existingOrder);
+                      console.log('check parents');
+                      console.log(newOrder);
+                      if(existingOrder.parentOrders.length > 0){
+                        console.log("this is:");
+                        console.log(this);
+                        this.checkRange(parentOrder);
+                        this.schedule.updateParents(existingOrder);
+                      }
+                    }
 };
 
-function ScheduleManager(){
-
-};
-
-ScheduleManager.prototype = {
-  parseSchedule: function(jsonData){
-    var myOrders = JSON.parse(orders);
-  }
-}
-
-// $(function() {
-//   console.log('workd');
-//   Renderer.init(myJSONData);
-//   Renderer.displayOrders();
-// });
+$(function() {
+  ScheduleOrganizer.init(myJSONData);
+});
